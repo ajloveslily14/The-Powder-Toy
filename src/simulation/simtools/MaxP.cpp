86ffe357595bda4942784304a1290766ffcc9b82
ajloveslily14 <ajloveslily@gmail.com>
2019-04-19 23:23:43 -0700

    add a max and min pressure tool

---
+++ src/simulation/simtools/MaxP.cpp
@@ -0,0 +1,18 @@
+#include "ToolClasses.h"
+#include "simulation/Air.h"
+//#TPT-Directive ToolClass Tool_MaxP TOOL_MAXP 99
+Tool_MaxP::Tool_MaxP(){
+	Identifier = "DEFAULT_TOOL_MAXP";
+	Name = "MAXP";
+	Colour = PIXPACK(0xFFFFFF);
+	Description = "MAXP, changes the pressure to the max pressure";
+}
+
+
+
+int Tool_MaxP::Perform(Simulation * sim, Particle * cpart, int x, int y, int brushX, int brushY, float strength){
+	sim->air->pv[y / CELL][x / CELL] = 256.0f;
+	return 1;
+}
+
+Tool_MaxP::~Tool_MaxP() {}
---
+++ src/simulation/simtools/MinP.cpp
@@ -0,0 +1,18 @@
+#include "ToolClasses.h"
+#include "simulation/Air.h"
+//#TPT-Directive ToolClass Tool_MinP TOOL_MINP 98
+Tool_MinP::Tool_MinP(){
+	Identifier = "DEFAULT_TOOL_MINP";
+	Name = "MINP";
+	Colour = PIXPACK(0x000000);
+	Description = "MINP, changes the pressure to the min pressure";
+}
+
+
+
+int Tool_MinP::Perform(Simulation * sim, Particle * cpart, int x, int y, int brushX, int brushY, float strength){
+	sim->air->pv[y / CELL][x / CELL] = -256.0f;
+	return 1;
+}
+
+Tool_MinP::~Tool_MinP() {}
